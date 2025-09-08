b830d7b951d47d952f6cd69bbc5dc692fff7120c
rotilho <felipe@rotilho.com>
2025-09-08 22:41:56 +0200

    Update `commonsVersion` to stable release 5.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ ext["kotlin-coroutines.version"] = "1.9.0"
 ext["kotlin-serialization.version"] = "1.8.0"
 
 dependencies {
-    val commonsVersion = "5.2.2-SNAPSHOT"
+    val commonsVersion = "5.2.2"
     val cucumberVersion = "7.23.0"
     val springdocVersion = "2.8.9"
 
