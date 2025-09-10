193ec89d9fac96e70de4f56e77bf35d2aa81a32e
rotilho <felipe@rotilho.com>
2025-09-10 09:41:35 +0200

    Add optional `limit` parameter to `getAll` in ReceivableController

--- src/main/kotlin/cash/atto/monitor/receivable/ReceivableController.kt
+++ src/main/kotlin/cash/atto/monitor/receivable/ReceivableController.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -28,5 +29,6 @@ class ReceivableController(
             ),
         ],
     )
-    fun getAll(): Collection<AttoReceivable> = receivableService.getReceivables()
+    fun getAll(@RequestParam(required = false) limit: Int = Int.MAX_VALUE): Collection<AttoReceivable> =
+        receivableService.getReceivables().take(limit)
 }
